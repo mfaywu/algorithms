15d68fb8601cadfebede1cf8243e6e4150e6f2cd
M. Fay Wu <mfaywu@ucla.edu>
2016-03-13 00:08:56 -0800

    Added optimised bubble sort for Python

--- Python/bubble_sort.py
+++ Python/bubble_sort.py
@@ -6,8 +6,22 @@ def bubble_sort (ls):
                 ls[i] = ls[i+1]
                 ls[i+1] = temp
 
-# TODO def bubble_sort_short (ls):
+def bubble_sort_o (ls):
+    swapped = False; 
+    for items in range (len (ls) - 1, 0, -1):
+        for i in range (items):
+            if ls[i] > ls[i+1]:
+                temp = ls[i]
+                ls[i] = ls[i+1]
+                ls[i+1] = temp
+                swapped = True
+        if swapped == False:
+            break
 
 ls = input ("Please enter array of numbers: ")
-bubble_sort (ls)
+choice = input ("0 regular; 1 optimized: " )
+if choice == 0:
+    bubble_sort (ls)
+else:
+    bubble_sort_o (ls)
 print (ls)
