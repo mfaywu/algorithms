9f4556ac7c60e7543e4c9064a4aec56c05d8333b
M. Fay Wu <mfaywu@ucla.edu>
2016-05-02 20:23:03 -0700

    Update Naive Pattern Searching (Python)

--- Python/naive_pattern_searching.py
+++ Python/naive_pattern_searching.py
@@ -1,22 +1,20 @@
-def bubble_sort (ls):
-    for items in range (len (ls) - 1, 0, -1):
-        for i in range (items):
-            if ls[i] > ls[i+1]:
-                temp = ls[i]
-                ls[i] = ls[i+1]
-                ls[i+1] = temp
+def match_pattern(text, pattern):
+    i = 0
+    while i < len(text):
+        if text[i] != pattern [i]:
+            return False
+        i = i + 1
+    return True
 
 def naive_search (text, pattern): 
     i = 0
-    j = 0
-    found = False
-    while (i < len(text)):
-        if text[i] == pattern[j]:
-            if j == len(pattern) - 1:
-                return i - len(pattern) + 1
-            j = j + 1
-        i = i + 1
-    return -1
+    idx = []
+    while i < (len(text) - len(pattern)):
+        if text[i] == pattern[0]:
+            if match_pattern(text[i:i+len(pattern)], pattern):
+                idx.append(i)
+        i = i+1
+    return idx
 
 text = input ("Please enter text: ")
 pattern = input ("Please enter pattern: " )
