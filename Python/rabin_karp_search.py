4700a940b3e1a79084864060ab3ead5040eeed9b
M. Fay Wu <mfaywu@ucla.edu>
2016-05-07 17:53:20 -0700

    Update Rabin Karp - incomplete

--- Python/rabin_karp_search.py
+++ Python/rabin_karp_search.py
@@ -1,3 +1,17 @@
 #Rabin Karp Algorithm
 #Common use: detecting plagiarism 
 #High # of matches, nonexact matching
+
+def rabin_karp_search (text, pattern):
+    hash_pattern = hash(pattern)
+    for i in range (0,  len(text) - len(pattern)):
+        hash_text = hash(text[i:i+len(pattern)])
+        if hash_text == hash_pattern:
+            for j in range(0, len(pattern)):
+                if text[i+j] != pattern[j]:
+                    break
+                print ("found at: "%d % (i))
+
+text = input ("Enter string text: ")
+pattern = input ("Enter pattern to search for: ")
+
