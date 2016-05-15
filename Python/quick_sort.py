31cd9d596dc83649e3eb133ee1078b227e127ee2
M. Fay Wu <mfaywu@ucla.edu>
2016-05-14 21:35:15 -0700

    Update: quick sort

--- Python/quick_sort.py
+++ Python/quick_sort.py
@@ -5,7 +5,15 @@ def quick_sort (ls, first, last):
         quick_sort (ls, pivot + 1, last)
 
 def partition (ls, first, last):
-    
+    pivot = ls[last]
+    i = first - 1
+
+    for i in range (first, last):
+        if ls[i] <= pivot:
+            j = j + 1
+            arr[j],arr[i] = arr[i],arr[j]
+    arr[j+1],arr[last] = arr[last], arr[j+1]
+    return j+1
 
 ls = input ("Please enter array of unsorted numbers: ")
 quick_sort (ls)
